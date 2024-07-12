10b8d84e3fd3e9e73a828f6ab7fb48219c9847ee
unknown <gregorydealmeida901@gmail.com>
2024-07-12 13:56:29 -0300

    Tudo em ordem

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
-import { require } from 'browserify';
 
 export default function Home() {
   return (
@@ -9,7 +8,7 @@ export default function Home() {
       <Link className="div-ds3" to="/ds3">
         <div>
           <h1>Dark Souls III</h1>
-          <img src={require('./src/components/home/img/ds3-img.jpg')} alt="" />
+          <img src="./src/components/home/img/ds3-img.jpg" alt="" />
           <h3>Clique para acessar os Clips</h3>
         </div>
       </Link>
@@ -17,7 +16,7 @@ export default function Home() {
       <Link className="div-ets2" to="/ets2">
         <div>
           <h1>Euro Truck Simulator 2</h1>
-          <img src="/src/components/home/img/ets2-img.jpg" alt="" />
+          <img src="./src/components/home/img/ets2-img.jpg" alt="" />
           <h3>Clique para acessar os Clips</h3>
         </div>
       </Link>

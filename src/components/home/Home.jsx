5ffc6d76c2c571267ea76aa395811ae0b86d0d18
unknown <gregorydealmeida901@gmail.com>
2024-07-12 13:48:04 -0300

    Required add

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import { require } from 'browserify';
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Link className="div-ds3" to="/ds3">
         <div>
           <h1>Dark Souls III</h1>
-          <img src="./src/components/home/img/ds3-img.jpg" alt="" />
+          <img src={require('./src/components/home/img/ds3-img.jpg')} alt="" />
           <h3>Clique para acessar os Clips</h3>
         </div>
       </Link>
